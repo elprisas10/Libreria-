de55530a5c102083ef24405c4c6c130b600c0077
elprisas10 <jonathangamezlarin@gmail.com>
2021-08-20 19:41:45 -0800

    Biblioteca

---
+++ LibreriaN/LibreriaN/SampleCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaN
+{
+    public class SampleCollection
+    {
+        public static void ListCollection()
+        {
+
+            List<object> lst = new List<object>();
+
+
+            var rs = "s";
+            while (rs == "s")
+            {
+                Console.Write("Escriba el elemento a agregar en la lista: ");
+                var element = Console.ReadLine();
+
+
+                lst.Add(element);
+
+
+                Console.Write("\nDesea agregar otro elemento? S/N ");
+                rs = Console.ReadLine();
+            }
+
+            {
+
+                Console.WriteLine("\n------------Elementos en la lista----------");
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    Console.WriteLine("Elemento {0}: {1}", i, lst[i]);
+                }
+
+            }
+        }
+    }
+}
+
---
+++ LibreriaUsoN/LibreriaUsoN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreriaN;
+
+namespace LibreriaUsoN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione un elemento: ");
+            Console.WriteLine("1. Crear Lista: ");
+
+
+            var response = Console.ReadLine();
+
+            if (response == "1")
+            {
+                SampleCollection.ListCollection();
+                Console.ReadKey();
+            }
+        }
+    }
+}
